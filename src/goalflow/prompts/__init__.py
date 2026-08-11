ca03bc89558857e12568a93135b5e445f928ee02
wangwei <wangwei2@wanlianyida.com>
2026-08-11 15:18:13 +0800

    chore: remove unused research_prompts module

--- src/goalflow/prompts/__init__.py
+++ src/goalflow/prompts/__init__.py
@@ -1,9 +1,4 @@
 from goalflow.prompts.prompts_templates import *
-from goalflow.prompts.research_prompts import (
-    INTENT_RECOGNITION_PROMPT,
-    TYPE_SPECIFIC_GUIDANCE,
-    FRAMEWORK_GENERATION_PROMPT_TEMPLATE,
-    RESEARCH_PLANNING_PROMPT_TEMPLATE
-)
+
 from goalflow.prompts.suggest_question_prompts import suggest_q_tpl_map
 
